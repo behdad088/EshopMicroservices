c7059ace7133acf9fa927050fad0a4aae9594afb
Behdad Kardgar <behdad.kardgar1@gmail.com>
2024-08-03 09:57:31 +0200

    add health check and basket repository

---
+++ src/Services/Basket/Basket.API/Common/HealthChecksExtension.cs
@@ -0,0 +1,20 @@
+using Basket.API.Configurations;
+using Basket.API.Configurations.ConfigurationOptions;
+using BuildingBlocks.HealthChecks;
+
+namespace Basket.API.Common;
+
+public static class HealthChecksExtension
+{
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddDefaultHealthChecks(configuration);
+        var databaseConfiguration = configuration.TryGetValidatedOptions<DatabaseConfigurations>();
+        
+        services.AddHealthChecks()
+            .AddNpgSql(databaseConfiguration.PostgresDb, name: "postgres", tags: ["ready", "liveness"])
+            .AddRedis(databaseConfiguration.Redis, "redis", tags: ["ready", "liveness"]);
+
+        return services;
+    }
+}
