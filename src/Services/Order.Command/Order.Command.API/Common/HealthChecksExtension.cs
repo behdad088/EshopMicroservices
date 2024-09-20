f6bc1982f385785a6afbce3a31c9afa0c2dd32f8
Behdad Kardgar <behdad.kardgar1@gmail.com>
2024-09-20 18:16:26 +0200

    Add order command api project (#46)

---
+++ src/Services/Order.Command/Order.Command.API/Common/HealthChecksExtension.cs
@@ -0,0 +1,14 @@
+using BuildingBlocks.HealthChecks;
+
+namespace Order.Command.API.Common;
+
+public static class HealthChecksExtension
+{
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddDefaultHealthChecks();
+        // should add db health check here
+        
+        return services;
+    }
+}
