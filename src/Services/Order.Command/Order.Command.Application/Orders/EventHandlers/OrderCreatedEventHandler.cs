42b35a2e9c7682ec4fc2ddcc86fd91907eff316e
Behdad Kardgar <behdad.kardgar1@gmail.com>
2024-10-09 08:50:03 +0200

    implement event handler order command

---
+++ src/Services/Order.Command/Order.Command.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+namespace Order.Command.Application.Orders.EventHandlers;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+{
+    public async Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain event handled: {DomanEvent}", notification.GetType());
+
+        await Task.CompletedTask;
+    }
+}
