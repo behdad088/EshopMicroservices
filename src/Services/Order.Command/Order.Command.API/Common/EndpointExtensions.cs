0ba027f16d8a5fdc32d60abfe02dc7be98a8328e
Behdad Kardgar <behdad.kardgar1@gmail.com>
2024-10-16 08:33:45 +0200

    fix

--- src/Services/Order.Command/Order.Command.API/Common/EndpointExtensions.cs
+++ src/Services/Order.Command/Order.Command.API/Common/EndpointExtensions.cs
@@ -8,10 +8,8 @@ public static class EndpointExtensions
 {
     public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
     {
-        ServiceDescriptor[] serviceDescriptors = assembly
-            .DefinedTypes
-            .Where(type => type is { IsAbstract: false, IsInterface: false } &&
-                           type.IsAssignableTo(typeof(IEndpoint)))
+        ServiceDescriptor[] serviceDescriptors = assembly.DefinedTypes
+            .Where(type => type is { IsAbstract: false, IsInterface: false } && type.IsAssignableTo(typeof(IEndpoint)))
             .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
             .ToArray();
 
