3343eb2953514e0dabcf8d8599819547a63e3da0
Behdad Kardgar <behdad.kardgar1@gmail.com>
2025-06-12 08:49:41 +0200

    fix formatting

--- src/Services/Identity/Identity.API/Pages/Index.cshtml.cs
+++ src/Services/Identity/Identity.API/Pages/Index.cshtml.cs
@@ -10,10 +10,10 @@ public class Index : PageModel
 {
     public Index(IdentityServerLicense? license = null) => License = license;
 
-    public string Version => typeof(Duende.IdentityServer.Hosting.IdentityServerMiddleware).Assembly
-                                 .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                                 ?.InformationalVersion.Split('+').First()
-                             ?? "unavailable";
+    public string Version =>
+        typeof(Duende.IdentityServer.Hosting.IdentityServerMiddleware).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Split('+').First() ??
+        "unavailable";
 
     public IdentityServerLicense? License { get; }
 }
