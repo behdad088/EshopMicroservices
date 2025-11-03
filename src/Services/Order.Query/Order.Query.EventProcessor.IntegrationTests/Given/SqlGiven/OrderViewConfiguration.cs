fc73792a3967936fae7aefc249c57424959e0e0d
Behdad Kardgar <behdad.kardgar1@gmail.com>
2025-11-03 08:31:44 +0100

    fix

--- src/Services/Order.Query/Order.Query.EventProcessor.IntegrationTests/Given/SqlGiven/OrderViewConfiguration.cs
+++ src/Services/Order.Query/Order.Query.EventProcessor.IntegrationTests/Given/SqlGiven/OrderViewConfiguration.cs
@@ -57,7 +57,7 @@ public class OrderViewConfiguration
     public int OrderUpdatedEventVersion  { get; set; } = 0;
     public int OrderDeletedEventVersion { get; set; } = 0;
 
-     public record OrderItem(
+    public record OrderItem(
         string Id,
         string ProductId,
         int Quantity,
